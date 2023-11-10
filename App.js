44c0db79a35781ecc69fe1f55b1e02e61f1ab060
JDLO <juandavidleono@gmail.com>
2023-11-10 14:24:06 +0100

    Enrutamiento de CreatePoliticPartyScreen y PoliticPartiesListScreen

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import CreateVoterScreen from "./screens/CreateVoterScreen";
 import VotersListScreen from "./screens/VotersListScreen";
+import PoliticPartiesListScreen from "./screens/PoliticPartiesListScreen";
+import CreatePoliticPartyScreen from "./screens/CreatePoliticPartyScreen";
 
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="CreatePoliticPartyScreen" component={CreatePoliticPartyScreen} />
+      <Stack.Screen name="PoliticPartiesListScreen" component={PoliticPartiesListScreen} />
       <Stack.Screen name="CreateVoterScreen" component={CreateVoterScreen} />
       <Stack.Screen name="VotersListScreen" component={VotersListScreen} />
     </Stack.Navigator>
