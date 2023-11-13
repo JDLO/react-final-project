19c7d3f51b332b7cca263e47c9292b07c11839bc
Luis Fernandez Suarez <uo271405@uniovi.es>
2023-11-13 13:59:06 +0100

    Nuevo atributo "emoji" en el componente "Partido"

--- screens/CreatePoliticPartyScreen.js
+++ screens/CreatePoliticPartyScreen.js
@@ -1,26 +1,33 @@
 import { View, Text, Button, TextInput, StyleSheet, ScrollView } from 'react-native';
-import React, { useState, setState } from 'react';
-
+import { useState, setState } from 'react';
+import * as RN from "react-native";
+import EmojiPicker from "rn-emoji-keyboard";
 import { database } from "../database/firebase";
 import { addDoc, collection } from "@firebase/firestore";
 
 const CreatePoliticPartyScreen = (props) => {
-    const [state, setState] = useState({
+    const [isOpen, setIsOpen] = useState(false);
+    const [newItem, setNewItem] = useState({
         nombre: '',
+        emoji: "📷",
     })
 
-    const saveNewUser = () => {
-        console.log(state);
+    const handlePick = (emojiObject) => {
+        setNewItem({
+            ...newItem,
+            emoji: emojiObject.emoji,
+        });
+    };
 
-        
-        if (state.nombre === '') {
+    const saveNewUser = () => {
+        if (newItem.nombre === '') {
             alert('Partido politico sin nombre');
             return;
-        } 
+        }
         // Si todo va bien, guardarlo en la base de datos
         const ref = collection(database, "partido");
         try {
-            addDoc(ref, state);
+            addDoc(ref, newItem);
         } catch (err) {
             console.log(err);
         }
@@ -33,8 +40,16 @@ const CreatePoliticPartyScreen = (props) => {
             <View style={styles.inputGroup}>
                 <Text>Crear Partido Político</Text>
             </View>
+            <RN.Text onPress={() => setIsOpen(true)} style={styles.emoji}>
+                {newItem.emoji}
+            </RN.Text>
+            <EmojiPicker
+                onEmojiSelected={handlePick}
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
             <View style={styles.inputGroup}>
-                <TextInput placeholder='Nombre' onChangeText={(value) => setState({ ...state, nombre: value })} />
+                <TextInput placeholder='Nombre' onChangeText={(value) => setNewItem({ ...newItem, nombre: value })} />
             </View>
             <View>
                 <Button title="Guardar Usuario" onPress={() => saveNewUser()} />
@@ -46,18 +61,32 @@ const CreatePoliticPartyScreen = (props) => {
 }
 
 
-const styles = StyleSheet.create({
+const styles = RN.StyleSheet.create({
     container: {
         flex: 1,
-        padding: 35
+        backgroundColor: "#fff",
+        alignItems: "center",
     },
-    inputGroup: {
-        flex: 1,
-        padding: 0,
-        marginBottom: 15,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc'
-    }
-})
+    title: {
+        fontSize: 32,
+        fontWeight: "700",
+    },
+    inputContainer: {
+        width: "90%",
+        padding: 13,
+        marginVertical: 6,
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 6,
+    },
+    emoji: {
+        fontSize: 100,
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 6,
+        padding: 10,
+        marginVertical: 6,
+    },
+});
 
 export default CreatePoliticPartyScreen;
