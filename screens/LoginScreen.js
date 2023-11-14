554ebe1374fc79c05c6048e9a98013f607c1e4fa
Luis Fernandez Suarez <uo271405@uniovi.es>
2023-11-14 13:43:44 +0100

    El botón de logout está en Home, ahora funciona

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,11 @@
 import * as RN from 'react-native';
-import React, { useState, setState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { auth } from '../database/firebase';
 
 import { database } from "../database/firebase";
 import { addDoc, collection } from "@firebase/firestore";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginScreen = (props) => {
     const [email, setEmail] = useState('');
@@ -41,16 +41,18 @@ const LoginScreen = (props) => {
         };
     }
 
-    const emailSignOut = async () => {
-        signOut()
-            .then(() => {
-                console.log('User account logged out!');
-                props.navigation.navigate('LoginScreen');
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+    // useEffect to clear the fields on component mount or when the user signs out
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (!user) {
+                // User is signed out, clear the fields
+                setEmail('');
+                setPassword('');
+            }
+        });
+
+        return () => unsubscribe();
+    }, []); // Empty dependency array means this effect runs once on mount
 
     return (
         <RN.View style={styles.container}>
@@ -71,7 +73,6 @@ const LoginScreen = (props) => {
             </RN.View>
             <RN.Button onPress={emailSignUp} title="Sign up" />
             <RN.Button onPress={emailSignIn} title="Sign in" />
-            <RN.Button onPress={emailSignOut} title="Sign out" />
         </RN.View>
     )
 }
