001c44c647685f15c245f0dfb68f70d648809325
JDLO <juandavidleono@gmail.com>
2023-11-16 10:13:59 +0100

    Cambio de iconos en el customButton

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,10 +1,11 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const CustomButton = ({ title, icon, onPress, style }) => (
     <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-        <FontAwesome name={icon} size={20} color="white" />
+        <Ionicons name={icon} size={20} color="white" />
         <Text style={styles.buttonText}>{title}</Text>
     </TouchableOpacity>
 );
